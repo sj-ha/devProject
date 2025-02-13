74afde7ba716996740743b8a8d3a2dec9c6aaf47
sj-ha <92284355+sj-ha@users.noreply.github.com>
2025-02-13 17:57:07 +0900

    250213 commit
    250213 commit

---
+++ Final_Boot00/src/main/java/com/example/demo/Hello.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class Hello {
+
+	public void Hello() {
+		System.out.println("하욤~321661565116");
+	}
+}
---
+++ Final_Spring00/Final_SQL.sql
@@ -0,0 +1,5 @@
+-- 계정 생성
+create identified final_admin by final_admin;
+-- 계정 권한 부여
+grant resource, connect to final_admin;
+-- 
